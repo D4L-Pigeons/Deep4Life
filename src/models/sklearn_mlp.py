61672814ab55acb3ad14398f878daafbff5ad6b0
adambiel22 <adam.biel22@gmail.com>
2024-05-08 21:03:48 +0200

    Change number of hidden layers to 2 in  sklearn_mlp

--- src/models/sklearn_mlp.py
+++ src/models/sklearn_mlp.py
@@ -12,7 +12,7 @@ class SklearnMLP(ModelBase):
         self.mlp_classifier = MLPClassifier(
             solver="adam",
             alpha=1e-5,
-            hidden_layer_sizes=(40 * 4, 40 * 4, 40 * 4),
+            hidden_layer_sizes=(40 * 4, 40 * 4),
             random_state=1,
             early_stopping=True,
             verbose=True,
