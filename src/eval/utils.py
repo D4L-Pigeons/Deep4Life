a16396e8c19cceea16ca1314d9398eab4fbf91f8
SzymonLukasik <szymon123lukasik@gmail.com>
2024-05-09 00:30:15 +0200

    Move metrics to seperate module

---
+++ src/eval/utils.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import anndata
+import torch
+from sklearn.model_selection import KFold
+from metrics import calculate_metrics
+from models.ModelBase import ModelBase
+
+def cross_validation(
+    data: anndata.AnnData, model: ModelBase, random_state: int = 42, n_folds: int = 5
+) -> pd.DataFrame:
+    torch.manual_seed(42)
+
+    metrics_names = [
+        "f1_score_per_cell_type",
+        "f1_score",
+        "accuracy",
+        "average_precision_per_cell_type",
+        "roc_auc_per_cell_type",
+        "confusion_matrix",
+    ]
+
+    cross_validation_metrics = pd.DataFrame(columns=metrics_names)
+
+    for i, (train_data, test_data) in enumerate(k_folds(data, n_folds, random_state)):
+        model.train(train_data)
+        prediction = model.predict(test_data)
+        prediction_probability = model.predict_proba(test_data)
+        ground_truth = test_data.obs["cell_labels"]
+
+        calculate_metrics(
+            ground_truth,
+            prediction,
+            prediction_probability,
+            test_data.obs["cell_labels"].cat.categories,
+            cross_validation_metrics,
+        )
+
+        print(
+            f"Validation accuracy of {i} fold:",
+            cross_validation_metrics.loc[i]["accuracy"],
+        )
+
+    average_metrics = {
+        metric_name: cross_validation_metrics[metric_name].mean()
+        for metric_name in metrics_names
+    }
+    cross_validation_metrics.loc[len(cross_validation_metrics.index)] = average_metrics
+
+    return cross_validation_metrics
+
+
+def k_folds(data: anndata.AnnData, n_folds: int, random_state: int):
+    sample_ids = data.obs["sample_id"].cat.remove_unused_categories()
+    sample_ids_unique = sample_ids.cat.categories
+
+    kfold = KFold(n_splits=n_folds, shuffle=True, random_state=random_state)
+    split = kfold.split(sample_ids_unique.tolist())
+
+    for train, test in split:
+        train_mask = data.obs["sample_id"].isin(sample_ids_unique[train])
+        test_mask = data.obs["sample_id"].isin(sample_ids_unique[test])
+
+        yield data[train_mask], data[test_mask]
--- src/train_and_validate.py
+++ src/train_and_validate.py
@@ -21,6 +21,7 @@ from models.torch_mlp import TorchMLP
 from models.sklearn_svm import SVMSklearnSVC
 from models.vanilla_stellar import VanillaStellarReduced
 from models.xgboost import XGBoostModel
+from utils import cross_validation
 
 CONFIG_PATH: Path = Path(__file__).parent / "config"
 RESULTS_PATH: Path = Path(__file__).parent.parent / "results"
@@ -121,134 +122,6 @@ def create_model(args, config) -> ModelBase:
         return SVMSklearnSVC(vars(config))
     else:
         raise NotImplementedError(f"{args.method} method not implemented.")
-
-
-def cross_validation(
-    data: anndata.AnnData, model: ModelBase, random_state: int = 42, n_folds: int = 5
-) -> pd.DataFrame:
-    torch.manual_seed(42)
-
-    metrics_names = [
-        "f1_score_per_cell_type",
-        "f1_score",
-        "accuracy",
-        "average_precision_per_cell_type",
-        "roc_auc_per_cell_type",
-        "confusion_matrix",
-    ]
-
-    cross_validation_metrics = pd.DataFrame(columns=metrics_names)
-
-    for i, (train_data, test_data) in enumerate(k_folds(data, n_folds, random_state)):
-        model.train(train_data)
-        prediction = model.predict(test_data)
-        prediction_probability = model.predict_proba(test_data)
-        ground_truth = test_data.obs["cell_labels"]
-
-        calculate_metrics(
-            ground_truth,
-            prediction,
-            prediction_probability,
-            test_data.obs["cell_labels"].cat.categories,
-            cross_validation_metrics,
-        )
-
-        print(
-            f"Validation accuracy of {i} fold:",
-            cross_validation_metrics.loc[i]["accuracy"],
-        )
-
-    average_metrics = {
-        metric_name: cross_validation_metrics[metric_name].mean()
-        for metric_name in metrics_names
-    }
-    cross_validation_metrics.loc[len(cross_validation_metrics.index)] = average_metrics
-
-    return cross_validation_metrics
-
-
-def k_folds(data: anndata.AnnData, n_folds: int, random_state: int):
-    sample_ids = data.obs["sample_id"].cat.remove_unused_categories()
-    sample_ids_unique = sample_ids.cat.categories
-
-    kfold = KFold(n_splits=n_folds, shuffle=True, random_state=random_state)
-    split = kfold.split(sample_ids_unique.tolist())
-
-    for train, test in split:
-        train_mask = data.obs["sample_id"].isin(sample_ids_unique[train])
-        test_mask = data.obs["sample_id"].isin(sample_ids_unique[test])
-
-        yield data[train_mask], data[test_mask]
-
-
-# def macro_average_precision(ground_truth, prediction_probability):
-#   """
-#   Calculates macro-averaged precision for multi-class classification.
-
-#   Args:
-#       ground_truth (array-like): Array of true labels.
-#       prediction_probability (array-like): Array of predicted class probabilities.
-
-#   Returns:
-#       float: Macro-averaged precision score.
-#   """
-#   num_classes = len(ground_truth.unique())
-
-#   precision_per_class = []
-
-#   # Calculate precision score for each class
-#   for class_label in range(num_classes):
-#     class_mask = ground_truth == class_label
-#     ground_truth_filtered = ground_truth[class_mask]
-#     prediction_probability_filtered = prediction_probability[class_mask]
-#     # Calculate precision for this class
-#     precision = sklearn.metrics.precision_score(ground_truth_filtered, prediction_probability_filtered[:, class_label], average='binary', zero_division=0)
-#     precision_per_class.append(precision)
-
-#   # Macro-average the precision scores
-#   macro_average_precision = np.mean(precision_per_class)
-#   return macro_average_precision
-
-
-
-def calculate_metrics(
-    ground_truth, prediction, prediction_probability, classes, cross_validation_metrics
-):
-    f1_score_per_cell_type = sklearn.metrics.f1_score(
-        ground_truth, prediction, labels=classes, average=None
-    )
-    f1_score = sklearn.metrics.f1_score(
-        ground_truth, prediction, labels=classes, average="macro"
-    )
-    accuracy = sklearn.metrics.accuracy_score(ground_truth, prediction)
-    if prediction_probability is not None:
-        average_precision_per_cell_type = sklearn.metrics.average_precision_score(
-            ground_truth, prediction_probability, average=None
-        )
-        roc_auc_per_cell_type = sklearn.metrics.roc_auc_score(
-            ground_truth,
-            prediction_probability,
-            multi_class="ovr",
-            average=None,
-            labels=classes,
-        )
-    else:
-        average_precision_per_cell_type = None
-        roc_auc_per_cell_type = None
-    confusion_matrix = sklearn.metrics.confusion_matrix(
-        ground_truth, prediction, labels=classes
-    )
-
-    metrics = {
-        "f1_score_per_cell_type": f1_score_per_cell_type,
-        "f1_score": f1_score,
-        "accuracy": accuracy,
-        "average_precision_per_cell_type": average_precision_per_cell_type,
-        "roc_auc_per_cell_type": roc_auc_per_cell_type,
-        "confusion_matrix": confusion_matrix,
-    }
-
-    cross_validation_metrics.loc[len(cross_validation_metrics.index)] = metrics
 
 
 if __name__ == "__main__":
